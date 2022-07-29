3fdb3ec746c6a48bfbc01156c1fb961d1e399880
Geonwoo Lee <gwlee@kakao.com>
2022-07-29 16:32:10 +0900

    프로세스/스케쥴 모달 추가

--- src/js/datepicker.js
+++ src/js/datepicker.js
@@ -1,1 +1,32 @@
-import { Datepicker } from "vanillajs-datepicker/js/Datepicker.js";
+import { easepick } from "@easepick/core";
+import { RangePlugin } from "@easepick/range-plugin";
+import { AmpPlugin } from "@easepick/amp-plugin";
+
+const start = document.querySelector("#start-date");
+const end = document.querySelector("#end-date");
+
+if (start !== null) {
+    const datePicker = new easepick.create({
+        element: start,
+        css: [
+            "https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.0/dist/index.css",
+        ],
+        zIndex: 10,
+        AmpPlugin: {
+            dropdown: {
+                months: true,
+                years: true,
+            },
+            resetButton: true,
+        },
+        RangePlugin: {
+            elementEnd: end,
+            locale: {
+                one: "일",
+                other: "일",
+            },
+        },
+        plugins: [AmpPlugin, RangePlugin],
+        lang: "ko-KR",
+    });
+}
